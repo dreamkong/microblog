ce9111cfd6792a64e147c2f5dd223cc3a87af8f8
dreamkong <dreamkong0113@gmail.com>
2019-03-27 23:19:55 +0800

    使用flask-mail+pyjwt发送重置密码邮件给用户

--- config.py
+++ config.py
@@ -14,11 +14,12 @@ class Config:
     # 邮箱
     MAIL_STATUS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['dreamkong0113@gmail.com']
 
     # 分页
     POSTS_PER_PAGE = 3
